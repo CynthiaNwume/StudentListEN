7080749bfbffc28e5ae3b1d41051b74e3320d8e1
ekenenwume <nwume@sheridancollege.ca>
2024-05-22 12:04:02 -0400

    fourth message

--- src/studentlist1/Student.java
+++ src/studentlist1/Student.java
@@ -5,7 +5,8 @@
 package studentlist1;
 
 /**
- *
+ * This class represents a student with a name and address.
+ * 
  * @author ekenenwume
  */
 public class Student {
@@ -13,25 +14,49 @@ public class Student {
     private String name;
     private String address;
 
-    public Student(String name) {
+    /**
+     * Constructor to initialize both name and address.
+     * 
+     * @param name the name of the student
+     * @param address the address of the student
+     */
+    public Student(String name, String address) {
         this.name = name;
-    }
-    public Student(String address){
         this.address = address;
     }
 
+    /**
+     * Gets the name of the student.
+     * 
+     * @return the name of the student
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Sets the name of the student.
+     * 
+     * @param name the new name of the student
+     */
     public void setName(String name) {
         this.name = name;
     }
     
+    /**
+     * Gets the address of the student.
+     * 
+     * @return the address of the student
+     */
     public String getAddress() {
         return address;
     }
 
+    /**
+     * Sets the address of the student.
+     * 
+     * @param address the new address of the student
+     */
     public void setAddress(String address) {
         this.address = address;
     }
